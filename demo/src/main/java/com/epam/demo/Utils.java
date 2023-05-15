b645c801329ef969d89286c44869f5833106f0bb
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-15 21:43:55 +0300

    1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,15 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+       return args.stream().allMatch(StringUtils::isPositiveNumber);
+        //args.stream().mapToInt(Integer::parseInt).allMatch(x->x>0);
     }
+
+
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        int s=Integer.parseInt(str);
+        return s > 0;
     }
 }
